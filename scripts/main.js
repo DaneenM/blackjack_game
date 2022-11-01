4e87f582cd83b9d4473cdad06cf8b6d491aeadd1
Josiah Lee <mays.d87@gmail.com>
2022-11-01 14:07:04 -0400

    blackjack game

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+
 const dealerHand = document.getElementById("dealer-hand");
 const playerHand = document.getElementById("player-hand");
 const deck = [];
@@ -19,5 +20,47 @@ for (let suit of suits) {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  // Execute after page load
-});
+
+})
+
+// for (s in suits) {
+//   for (r in ranks) {
+//     // dealer-hand.innerHTML += "&" + suits[s] + ";" + ranks[r] + " ";
+//     // console.log(suits[s]+rank[r]);
+//   }
+// }
+function shuffleDeck() {
+  let randonNum = Math.floor((math.random() * 52));
+  console.log(deck[randonNum])
+}
+
+shuffleDeck ();
+
+// dealButton.addEventListener("click", event => dealCards())
+
+// function shuffleDeck() {
+//   for (let i = 0; i < deck.length; i++)
+//     let j = Math.floor(Math.random() * deck.length);
+//   let temp = deck[i];
+//   deck[i] = deck[j];
+//   deck[j] = temp;
+// }
+// console.log(deck);
+
+// function makeDeck(card) {
+//   let data = card.split()
+// }
+
+
+// function buildDeck() {
+//   let values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+//   let types = ["hearts", "spades", "clubs", "diamonds"];
+//     deck = [];
+
+//     for (let i = 0; i < types.length; i++) {
+//       for (let j = 0; j < values.length; j++) {
+//         deck.push(values[j] + "_" + types[i]);
+//       }
+//     }
+//     console.log(deck);
+// })
